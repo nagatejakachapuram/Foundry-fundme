b12f90b7f587f42910b2cea73c335c2b48c31b91
Nagateja kachapuram <104291208+nagatejakachapuram@users.noreply.github.com>
2024-12-29 22:48:21 +0530

    Update Fundme.sol
    Added getters

--- src/Fundme.sol
+++ src/Fundme.sol
@@ -24,7 +24,7 @@ contract FundMe {
         i_owner = msg.sender;
         s_priceFeed = AggregatorV3Interface(priceFeed);
     }
-
+    // Funding function
     function fund() public payable {
         require(msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD, "You need to spend more ETH!");
         // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
@@ -35,13 +35,13 @@ contract FundMe {
     function getVersion() public view returns (uint256) {
         return s_priceFeed.version();
     }
-
+        
     modifier onlyOwner() {
         // require(msg.sender == owner);
         if (msg.sender != i_owner) revert NotOwner();
         _;
     }
-
+    // Withdraw function
     function withdraw() public onlyOwner {
         for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
             address funder = funders[funderIndex];
@@ -78,13 +78,19 @@ contract FundMe {
     receive() external payable {
         fund();
     }
+ /** 
+     * View / Pure functions(getters)
+     */
+    function getAddressToAmountFunded(address fundingAddress) external view returns(uint256){
+        return addressToAmountFunded[fundingAddress];
+    }
+
+    function getFunder(uint256 index) external view returns(address){
+        return funders[index];
+    }
+
+    function getOwner() external view returns(address){
+        return i_owner;
+    }
 }
 
-// Concepts we didn't cover yet (will cover in later sections)
-// 1. Enum
-// 2. Events
-// 3. Try / Catch
-// 4. Function Selector
-// 5. abi.encode / decode
-// 6. Hash with keccak256
-// 7. Yul / Assembly
